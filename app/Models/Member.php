b584b6f60387ee62bcee170ecc53d4e56468803c
AlexLg <alexlegras@hotmail.com>
2025-07-27 17:19:51 +0200

    Add visible property to member model

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -2,13 +2,28 @@
 
 namespace App\Models;
 
+use Database\Factories\MemberFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Member extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
+        'first_name',
+        'last_name',
+        'joined_at',
+        'is_minor',
+        'created_at',
+        'updated_at',
+    ];
+
+    protected $visible = [
+        'id',
         'first_name',
         'last_name',
         'joined_at',
@@ -20,17 +35,21 @@ class Member extends Model
     /**
      * @return hasMany<Contact, $this>
      */
-    public function contact(): HasMany
+    public function contacts(): HasMany
     {
         return $this->hasMany(Contact::class);
     }
 
     /**
-     * @return BelongsToMany <Contact, $this>
+     * @return BelongsToMany <Material, $this>
      */
-    // public function borrowed(): BelongsToMany
-    // {
-    //     return $this->belongsToMany(Material::class, 'material_member')
-    //         ->withPivot(['borrowed_at', 'returned_at']);
-    // }
+    public function borrowed(): BelongsToMany
+    {
+        return $this->belongsToMany(Material::class, 'material_member')
+            ->withPivot(['borrowed_at', 'returned_at']);
+    }
+
+    protected static function newFactory() : Factory {
+        return new MemberFactory();
+    }
 }
